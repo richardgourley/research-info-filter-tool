9b4ccd500dedb3c30b4683065bcf4e526b9601ca
Rich <codeleap@outlook.com>
2020-09-26 20:42:11 +0200

    Test file test added.

--- tests.py
+++ tests.py
@@ -7,5 +7,10 @@ class UnitTests(unittest.TestCase):
         expected = True
         self.assertEqual(actual, expected, 'Expected True.')
 
+    def test_file_opened(self):
+    	actual = main.tempFile
+    	expected = True
+    	self.assertEqual(actual, expected, 'Expected a tempfile.txt file to have been created')
+
 if __name__ == "__main__":
     unittest.main()
