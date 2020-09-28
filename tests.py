582eb15b1232f9bf6b18558bd49e646bfc35e361
Rich <codeleap@outlook.com>
2020-09-28 20:10:57 +0200

    Initial tests removed.

--- tests.py
+++ tests.py
@@ -2,15 +2,7 @@ import unittest
 import main
 
 class UnitTests(unittest.TestCase):
-    def test_enter_urls_exists(self):
-        actual = main.enterUrls
-        expected = True
-        self.assertEqual(actual, expected, 'Expected True.')
-
-    def test_file_opened(self):
-    	actual = main.tempFile
-    	expected = True
-    	self.assertEqual(actual, expected, 'Expected a tempfile.txt file to have been created')
-
+    pass
+    
 if __name__ == "__main__":
     unittest.main()
