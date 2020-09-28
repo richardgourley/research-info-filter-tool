6c39d8dc5e92c021179bf76ca67c962a08cfeb04
Rich <codeleap@outlook.com>
2020-09-28 20:14:31 +0200

    Short form url test added and test for page url added.

--- tests.py
+++ tests.py
@@ -14,5 +14,29 @@ class UnitTests(unittest.TestCase):
         expected = True
         self.assertEqual(actual, expected, "Expected 'http://www.testsite.com' to return True.")
 
+    def testCheckUrlHttps(self):
+        researchTool = researchtool.ResearchTool([], 'flowers')
+        actual = researchTool.checkUrl('https://www.testsite.com')
+        expected = True
+        self.assertEqual(actual, expected, "Expected 'https://www.testsite.com' to return True.")
+
+    def testCheckUrlShort(self):
+        researchTool = researchtool.ResearchTool([], 'flowers')
+        actual = researchTool.checkUrl('https://testsite.com')
+        expected = True
+        self.assertEqual(actual, expected, "Expected 'https://testsite.com' to return True.")
+
+    def testCheckUrlInvalid(self):
+        researchTool = researchtool.ResearchTool([], 'flowers')
+        actual = researchTool.checkUrl('https://testsite.c.om')
+        expected = False
+        self.assertEqual(actual, expected, "Expected 'https://wwww.testsite.c.om' to return False.")
+
+    def testCheckUrlPage(self):
+        researchTool = researchtool.ResearchTool([], 'flowers')
+        actual = researchTool.checkUrl('https://testsite.com/new_book')
+        expected = True
+        self.assertEqual(actual, expected, "Expected 'https://testsite.com/new_book' to return True.")
+
 if __name__ == "__main__":
     unittest.main()
