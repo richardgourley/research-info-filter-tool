0b38a3a74444b3a23059426127fe0baac6626e88
Rich <codeleap@outlook.com>
2020-09-28 20:13:31 +0200

    Http url test added.

--- tests.py
+++ tests.py
@@ -7,6 +7,12 @@ class UnitTests(unittest.TestCase):
         actual = researchTool.displayWebsiteList()
         expected = "1234\n4567\n"
         self.assertEqual(actual, expected, "Expected '1234\n4567\n' returned as a string.")
-    
+
+    def testCheckUrlHttp(self):
+        researchTool = researchtool.ResearchTool([], 'flowers')
+        actual = researchTool.checkUrl('http://www.testsite.com')
+        expected = True
+        self.assertEqual(actual, expected, "Expected 'http://www.testsite.com' to return True.")
+
 if __name__ == "__main__":
     unittest.main()
