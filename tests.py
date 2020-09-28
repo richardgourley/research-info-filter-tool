06751cc9fafff95b6a4ffb15677c986cd7ffd881
Rich <codeleap@outlook.com>
2020-09-28 20:20:15 +0200

    Display list of websites test removed.

--- tests.py
+++ tests.py
@@ -1,13 +1,7 @@
 import unittest
-import main
+import researchtool
 
 class UnitTests(unittest.TestCase):
-    def testDisplayWebsiteList(self):
-        researchTool = researchtool.ResearchTool(['1234', '4567'], 'flowers')
-        actual = researchTool.displayWebsiteList()
-        expected = "1234\n4567\n"
-        self.assertEqual(actual, expected, "Expected '1234\n4567\n' returned as a string.")
-
     def testCheckUrlHttp(self):
         researchTool = researchtool.ResearchTool([], 'flowers')
         actual = researchTool.checkUrl('http://www.testsite.com')
@@ -37,6 +31,7 @@ class UnitTests(unittest.TestCase):
         actual = researchTool.checkUrl('https://testsite.com/new_book')
         expected = True
         self.assertEqual(actual, expected, "Expected 'https://testsite.com/new_book' to return True.")
+
 
 if __name__ == "__main__":
     unittest.main()
