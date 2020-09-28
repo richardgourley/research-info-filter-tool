2529f30005ec27d25b326f47c413fadbd3ab28fc
Rich <codeleap@outlook.com>
2020-09-28 20:12:07 +0200

    Website list display tested.

--- tests.py
+++ tests.py
@@ -2,7 +2,11 @@ import unittest
 import main
 
 class UnitTests(unittest.TestCase):
-    pass
+    def testDisplayWebsiteList(self):
+        researchTool = researchtool.ResearchTool(['1234', '4567'], 'flowers')
+        actual = researchTool.displayWebsiteList()
+        expected = "1234\n4567\n"
+        self.assertEqual(actual, expected, "Expected '1234\n4567\n' returned as a string.")
     
 if __name__ == "__main__":
     unittest.main()
