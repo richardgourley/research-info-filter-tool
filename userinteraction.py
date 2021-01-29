9a6ccc33fbc3e2bf009412aa91aa5b85b24db43b
Rich <codeleap@outlook.com>
2021-01-29 19:12:20 +0100

    UserInteraction class added to classes/

--- userinteraction.py
+++
@@ -1,9 +0,0 @@
-'''
-This class interacts with the user, providing messages
-The class takes an instance of the ResearchTool class
-The methods in the class aim to make it easier to break down at which point in the process ...
-..the user is at
-'''
-class UserInteraction:
-	def _init__(self, research_tool):
-		self.research_tool = research_tool
