f6bf5ee25639d1f7a45e788acebba172f0031612
Rich <codeleap@outlook.com>
2021-01-30 17:12:08 +0100

    Import error fixed

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-from classes import filehandler
-from classes import userinteraction
-from classes import urlrequester
-
+from classes.filehandler import FileHandler
+from classes.userinteraction import UserInteraction
+from classes.urlrequester import UrlRequester
 
 #### Class with methods to create directories, open files, write to files
-file_handler = filehandler.FileHandler()
+file_handler = FileHandler()
 
 #### Class that utilizes 'requests' and 'Beautiful Soup' libraries
-url_requester = urlrequester.UrlRequester()
+url_requester = UrlRequester()
 
 #### Class that gets input from user - uses FileHandler and UrlRequester classes
-user_interaction = userinteraction.UserInteraction(file_handler, url_requester)
+user_interaction = UserInteraction(file_handler, url_requester)
 
 ### RUN THE PROGRAM ###
 user_interaction.start()
