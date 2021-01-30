c6972adf423858fc52b57a8c0b2bdff7eb448ef6
Rich <codeleap@outlook.com>
2021-01-30 17:14:46 +0100

    open_file_write mode method added

--- classes/filehandler.py
+++ classes/filehandler.py
@@ -31,10 +31,14 @@ class FileHandler():
     def open_file_read(self, file_name):
         return open(file_name, "r")
 
+    def open_file_write(self, file_name):
+        return open(file_name, "w")
+
     '''
     Creates 'tempfile.txt' in append binary mode
     '''
     def create_temporary_file_append_binary(self):
         # must be append binary to work with UrlRequester class
         return open("tempfile.txt", "ab")
+
             
