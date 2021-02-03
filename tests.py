0206c04914a36f9cd1a5ca3bf392246e723c2dff
Rich <codeleap@outlook.com>
2021-02-03 11:53:37 +0100

    Tests for research tool class removed

--- tests.py
+++ tests.py
@@ -2,47 +2,7 @@ import unittest
 import researchtool
 
 class UnitTests(unittest.TestCase):
-    def testCheckUrlHttp(self):
-        researchTool = researchtool.ResearchTool('flowers')
-        actual = researchTool.checkUrl('http://www.testsite.com')
-        expected = True
-        self.assertEqual(actual, expected, "Expected 'http://www.testsite.com' to return True.")
-
-    def testCheckUrlHttps(self):
-        researchTool = researchtool.ResearchTool('flowers')
-        actual = researchTool.checkUrl('https://www.testsite.com')
-        expected = True
-        self.assertEqual(actual, expected, "Expected 'https://www.testsite.com' to return True.")
-
-    def testCheckUrlShort(self):
-        researchTool = researchtool.ResearchTool('flowers')
-        actual = researchTool.checkUrl('https://testsite.com')
-        expected = True
-        self.assertEqual(actual, expected, "Expected 'https://testsite.com' to return True.")
-
-    def testCheckUrlInvalid(self):
-        researchTool = researchtool.ResearchTool('flowers')
-        actual = researchTool.checkUrl('https://testsite.c.om')
-        expected = False
-        self.assertEqual(actual, expected, "Expected 'https://wwww.testsite.c.om' to return False.")
-
-    def testCheckUrlPage(self):
-        researchTool = researchtool.ResearchTool('flowers')
-        actual = researchTool.checkUrl('https://testsite.com/new_book')
-        expected = True
-        self.assertEqual(actual, expected, "Expected 'https://testsite.com/new_book' to return True.")
-
-    def testCreateTopicTextFileName(self):
-        researchTool = researchtool.ResearchTool('looking after flowers')
-        actual = researchTool.createTopicTextFileName()
-        expected = "looking_after_flowers.txt"
-        self.assertEqual(actual, expected, "Expected 'looking_after_flowers.txt' from topic name.")
-    
-    def testCreateFile(self):
-        researchTool = researchtool.ResearchTool('testing')
-        actual = researchTool.createFile()
-        expected = "We have created a file called 'testing.txt' to store your paragraphs in."
-        self.assertEqual(actual, expected, "Expected message - we have created a file.....'testing.txt'")
+    pass
 
 
 if __name__ == "__main__":
