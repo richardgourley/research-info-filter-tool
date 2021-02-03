8bfb8012822bac860e43c23f95f49436b740d8ed
Rich <codeleap@outlook.com>
2021-02-03 12:01:05 +0100

    make_directory method test added

--- tests.py
+++ tests.py
@@ -2,7 +2,14 @@ import unittest
 import researchtool
 
 class UnitTests(unittest.TestCase):
-    pass
+    '''
+    File handler tests
+    '''
+    def test_make_directory_returns_true(self):
+        file_handler = filehandler.FileHandler()
+        can_open_file = file_handler.make_directory('test')
+        expected = True
+        self.assertEqual(can_open_file, expected, "Expected a successful opening of a file to return true.")
 
 
 if __name__ == "__main__":
