8a13e0fe10f7a0bedec062e1cc496779c3acc926
Rich <codeleap@outlook.com>
2021-02-09 16:28:33 +0100

    File handler instance added to class @classmethod

--- tests.py
+++ tests.py
@@ -1,30 +1,28 @@
 import unittest
-import researchtool
+from classes import filehandler
+import os
 
 class UnitTests(unittest.TestCase):
     @classmethod
     def setUp(self):
         self.directory_name = 'test'
-    
+        self.file_handler = filehandler.FileHandler()
     '''
     File handler tests
     '''
     def test_make_directory_returns_true(self):
-        file_handler = filehandler.FileHandler()
-        can_open_file = file_handler.make_directory('test')
+        can_open_file = self.file_handler.make_directory(self.directory_name)
         expected = True
         self.assertEqual(can_open_file, expected, "Expected a successful opening of a file to return true.")
 
     def test_create_file_returns_true(self):
-        file_handler = filehandler.FileHandler()
         # Expecting 'test' dir to exist
-        can_create_file = file_handler.create_file('linux', self.directory_name)
+        can_create_file = self.file_handler.create_file('linux', self.directory_name)
         expected = True
         self.assertEqual(can_create_file, expected, "Expected a successful creation of a file.")
 
     def test_open_file_append_returns_file_mode_a(self):
-        file_handler = filehandler.FileHandler()
-        file = file_handler.open_file_append('test/linux')
+        file = self.file_handler.open_file_append('test/linux')
         mode = file.mode
         expected = "a"
         file.close()
