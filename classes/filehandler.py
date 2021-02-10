9e1b0bf9b588aea6120dabe607a8d86b535efe22
Rich <codeleap@outlook.com>
2021-02-10 07:42:35 +0100

    create_file renamed as create_file_in_directory

--- classes/filehandler.py
+++ classes/filehandler.py
@@ -13,7 +13,7 @@ class FileHandler():
                 return None
         return True
 
-    def create_file(self, topic_file_name, directory_name):
+    def create_file_in_directory(self, topic_file_name, directory_name):
         try:
             os.chdir(directory_name)
             results_file = open(topic_file_name, "w")
@@ -21,24 +21,5 @@ class FileHandler():
             return True
         except:
             return None
-
-    '''
-    Opens topic file in append mode
-    '''
-    def open_file_append(self, file_name):
-        return open(file_name, "a")
-
-    def open_file_read(self, file_name):
-        return open(file_name, "r")
-
-    def open_file_write(self, file_name):
-        return open(file_name, "w")
-
-    '''
-    Creates 'tempfile.txt' in append binary mode
-    '''
-    def create_temporary_file_append_binary(self):
-        # must be append binary to work with UrlRequester class
-        return open("tempfile.txt", "ab")
 
             
