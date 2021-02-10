0b33cd639dfdf8d3a9f96e912d5d872b90ca22ea
Rich <codeleap@outlook.com>
2021-02-10 07:44:03 +0100

    File handler tests - open, append write file tests removed - no longer class methods

--- tests.py
+++ tests.py
@@ -34,4 +34,30 @@ class UnitTests(unittest.TestCase):
         self.assertEqual(file.mode, expected, "Expected file in 'ab' mode to be opened.")
 
 if __name__ == "__main__":
+    unittest.main()import unittest
+from classes import filehandler
+import os
+
+class UnitTests(unittest.TestCase):
+    @classmethod
+    def setUp(self):
+        self.directory_name = 'test'
+        self.file_handler = filehandler.FileHandler()
+    '''
+    File handler tests
+    '''
+    def test_make_directory_returns_true(self):
+        can_open_file = self.file_handler.make_directory(self.directory_name)
+        expected = True
+        self.assertEqual(can_open_file, expected, "Expected a successful opening of a file to return true.")
+
+    def test_create_file_returns_true(self):
+        # Expecting 'test' dir to exist
+        can_create_file = self.file_handler.create_file('linux', self.directory_name)
+        expected = True
+        self.assertEqual(can_create_file, expected, "Expected a successful creation of a file.")
+
+
+
+if __name__ == "__main__":
     unittest.main()
